9a63949973664a82f3808e5f78ecc2e0c7c100fe
Natalia <davydenko.n@ukr.net>
2022-09-12 20:36:20 +0300

    done 3 home task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onPromisesCreate);
+
+function onPromisesCreate(event) {
+  event.preventDefault();
+
+  let delay = Number(form.delay.value);
+  let step = Number(form.step.value);
+  let amount = Number(form.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
